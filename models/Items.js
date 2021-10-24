b06304368ec6d9c839a3334cfec13512a2adaea2
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-10-24 00:52:12 -0300

    añade modelos de la base de datos

---
+++ models/Items.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const itemsSchema = mongoose.Schema({
+    nameProduct:{
+        type:String,
+        required: true
+    },
+    category:{
+        type:String,
+        required: true
+    },
+    subCategory:{
+        type:String,
+        required: true
+    },
+    description:{
+        type:String,
+        required: true
+    },
+    price:{
+        type:Number,
+        required: true
+    },
+    stock:{
+        type:Number,
+        required: true,
+        default: 0
+    },
+    discount:{
+        type:Number,
+        required: true
+    },
+    offer:{
+        type:Boolean,
+        required: true
+    },
+    productFeatured: {
+        type:Boolean,
+        required: true
+    },
+    available:{
+        type: Boolean,
+        required: true,
+        default: true
+    }
+
+});
+
+module.exports = mongoose.model("items", itemsSchema);
