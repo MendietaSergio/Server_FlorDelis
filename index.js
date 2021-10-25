ee7f324e3ca85e6e4ef6f908ba3be1c616f66751
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-10-25 17:51:39 -0300

    Añade configuración de cors

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require('dotenv').config()
+require("dotenv").config();
 const cors = require("cors");
+const config = {
+  application: {
+    cors: {
+      server: [
+        {
+          origin: "localhost:3000", //servidor que deseas que consuma o (*) en caso que sea acceso libre
+          credentials: true,
+        },
+      ],
+    },
+  },
+};
 const routes = require("./routes");
 const app = express();
 //habilitar body parser
@@ -28,4 +40,11 @@ app.use(cors());
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Servidor corriendo en ", 500);
+});
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
 });
